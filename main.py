b8d75a19b9b5d2fdf57ed702373c8e9f3f98e701
Zeinal Abidin <zaenal.abidin02017@gmail.com>
2025-11-27 00:18:07 +0800

    memperbaiki alur main.py

--- main.py
+++ main.py
@@ -7,23 +7,39 @@ def main():
     print("=== SISTEM PENJUALAN TOKO FURNITURE ===\n")
 
     while True:
-        user = login()
-        if not user:
-            print("Gagal login. Program keluar.")
+        print("Menu Utama")
+        print("1. Login")
+        print("2. Keluar")
+
+        pilihan = input("Pilih menu: ").strip()
+
+        if pilihan == "2":
+            print("Program selesai.")
             break
 
-        role = user["role"]
+        elif pilihan == "1":
+            user = login()
 
-        if role == "admin":
-            admin_menu()
-        elif role == "inventory":
-            menu_inventory()
-        elif role == "pembeli":
-            menu_pembeli()
+            if not user:
+                print("Sisa percobaan habis. Login gagal.\n")
+                continue
+
+            role = user["role"]
+
+            if role == "admin":
+                admin_menu()
+            elif role == "inventory":
+                menu_inventory()
+            elif role == "pembeli":
+                menu_pembeli()
+            else:
+                print("Ada yang error.\n")
+
+            print("Logout berhasil.\n")
+
         else:
-            print("Role tidak dikenal.")
+            print("Pilihan tidak valid.\n")
 
-        print("\nLogout berhasil.\n")
 
 if __name__ == "__main__":
     main()
